6c090c59ad333f8b39509664de1e477de2775dcc
Gadest <114533276+Gadest@users.noreply.github.com>
2023-07-03 01:37:37 +0300

    completed

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -5,18 +5,18 @@ import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
-Set<String> combine = new HashSet<>();
+        Set<String> combine = new HashSet<>();
         Set<String> result = new HashSet<>();
-combine.addAll(secondSet);
-combine.addAll(firstSet);
+        combine.addAll(secondSet);
+        combine.addAll(firstSet);
         for (String s:
-             combine) {
+                combine) {
             if(secondSet.contains(s)==true && firstSet.contains(s)==true){
-               result.add(s);
+                result.add(s);
             }
         }
         for (String s:
-             thirdSet) {
+                thirdSet) {
             if (result.contains(s)==true ||secondSet.contains(s)==true || firstSet.contains(s)==true){
                 result.remove(s);
             }
